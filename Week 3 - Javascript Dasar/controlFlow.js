791f7515ff0f2257e0e0af9060ff75498d7ada51
awaludin-dev <ubedsf@gmail.com>
2021-10-30 18:05:43 +0800

    add some conditional to controlFlow

--- Week 3 - Javascript Dasar/controlFlow.js
+++ Week 3 - Javascript Dasar/controlFlow.js
@@ -1,15 +1,24 @@
-let nilai = prompt(`Masukkan nilai anda`);
+let ulang;
 
-if (nilai < 0 || nilai > 100) {
-    alert(`Masukkan nilai dengan benar`);
-} else if (nilai > 80) {
-    alert(`Nilai anda A`);
-} else if (nilai > 70 && nilai <= 80) {
-    alert(`Nilai anda B`);
-} else if (nilai > 60 && nilai <= 70) {
-    alert(`Nilai anda C`);
-} else if (nilai > 50 && nilai <= 60) {
-    alert(`Nilai anda D`);
-} else {
-    alert(`Nilai anda E`);
+while(ulang!=1){
+    let nilai = prompt(`Masukkan nilai anda`);
+    if (nilai < 0 || nilai > 100) {
+        alert(`Masukkan nilai dengan benar`);
+        ulang = 0;
+    } else if (nilai > 80) {
+        alert(`Nilai anda A`);
+        ulang = 1;
+    } else if (nilai > 70 && nilai <= 80) {
+        alert(`Nilai anda B`);
+        ulang = 1;
+    } else if (nilai > 60 && nilai <= 70) {
+        alert(`Nilai anda C`);
+        ulang = 1;
+    } else if (nilai > 50 && nilai <= 60) {
+        alert(`Nilai anda D`);
+        ulang = 1;
+    } else {
+        alert(`Nilai anda E`);
+        ulang = 1;
+    }
 }
